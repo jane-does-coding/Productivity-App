690bc02238d58d72d1aa5147f9910d5c1338acaf
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-02-29 19:10:14 -0700

    nav done

--- app/components/Navbar/MenuItem.tsx
+++ app/components/Navbar/MenuItem.tsx
@@ -9,7 +9,7 @@ const MenuItem = ({ onClick, label }: MenuItemProps) => {
   return (
     <div
       onClick={onClick}
-      className="px-4 py-3 hover:bg-neutral-100/[50%] transition backdrop-blur-md"
+      className="px-4 py-3 hover:bg-neutral-100/[50%] font-semibold transition backdrop-blur-md"
     >
       {label}
     </div>
--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -6,6 +6,9 @@ import UserMenu from "./UserMenu";
 import { User } from "@prisma/client";
 
 import { RiTimerLine } from "react-icons/ri";
+import { IoSettingsOutline } from "react-icons/io5";
+import { LuListTodo } from "react-icons/lu";
+import { SlNotebook } from "react-icons/sl";
 
 interface NavbarProps {
   currentUser?: User | null;
@@ -13,16 +16,21 @@ interface NavbarProps {
 
 const Navbar = ({ currentUser }: NavbarProps) => {
   return (
-    <nav className="fixed w-full lg:w-[50vw] lg:rounded-full ml-0 lg:ml-[25vw] bg-neutral-100/25 z-10 top-4 p-2 backdrop-blur-md">
-      <div className="">
-        <Container>
-          <div className="flex flex-row items-center justify-center gap-3 md:gap-0">
-            <div className="p-2 md:py-3 md:px-3 transition flex flex-row  items-center gap-3 rounded-full cursor-pointer hover:bg-neutral-100/25">
-              <RiTimerLine size={28} />
-            </div>
-            <UserMenu currentUser={currentUser} />
-          </div>
-        </Container>
+    <nav className="fixed w-full lg:w-[50vw] lg:rounded-full ml-0 lg:ml-[25vw] bg-neutral-100/[35%] z-10 top-4 p-2 backdrop-blur-md gap-8">
+      <div className="flex flex-row items-center justify-center gap-3 ">
+        <div className="p-2 md:py-3 md:px-3 transition flex flex-row  items-center gap-3 rounded-full cursor-pointer hover:bg-neutral-100/25">
+          <SlNotebook size={28} />
+        </div>
+        <div className="p-2 md:py-3 md:px-3 transition flex flex-row  items-center gap-3 rounded-full cursor-pointer hover:bg-neutral-100/25">
+          <LuListTodo size={28} />
+        </div>
+        <div className="p-2 md:py-3 md:px-3 transition flex flex-row  items-center gap-3 rounded-full cursor-pointer hover:bg-neutral-100/25">
+          <RiTimerLine size={28} />
+        </div>
+        <div className="p-2 md:py-3 md:px-3 transition flex flex-row  items-center gap-3 rounded-full cursor-pointer hover:bg-neutral-100/25">
+          <IoSettingsOutline size={28} />
+        </div>
+        <UserMenu currentUser={currentUser} />
       </div>
     </nav>
   );
