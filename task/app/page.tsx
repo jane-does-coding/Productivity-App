a37a1ab7d5faa403fc9e067bc28fe5db57a42c01
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-03-02 16:06:07 -0700

    bg video done

--- task/app/page.tsx
+++ task/app/page.tsx
@@ -1,17 +1,25 @@
 "use client";
 import Timer from "./components/Timer";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="text-white bg-black/[15%] h-screen w-full overflow-hidden flex items-center justify-center">
-      <Image
+      {/* <Image
         alt="image"
         src={"/wallpaper1.jpg"}
         width={3000}
         height={3000}
         className="w-full h-screen fixed top-0 left-0 z-[-1] object-cover"
-      />
+      /> */}
+      <video
+        className="w-full h-screen fixed top-0 left-0 z-[-1] object-cover"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/ocean2.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
       <Timer />
     </main>
   );
